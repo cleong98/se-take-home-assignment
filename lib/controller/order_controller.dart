3a79e680bed4868e9830607bbb449f9ddf557927
cleong98 <zhuzhuloh1998@gmail.com>
2022-09-19 20:26:16 +0800

    refactor order controller 1. refactor robot handle order without create new isolate. 2. refactor reset order function

--- lib/controller/order_controller.dart
+++ lib/controller/order_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:isolate';
 
 import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,11 +10,12 @@ import '../models/order/vip_order.dart';
 import '../models/robot/basic_robot.dart';
 import '../models/robot/mcd_robot.dart';
 
-
 class OrderController extends ChangeNotifier {
   final List<BasicOrder> _queue = [];
   int _vipOrderTotal = 0;
   final List<BasicRobot> _robotList = [];
+  Timer? _timer;
+  final int _handleTimes = 10;
 
   // add normal order
   void addNormalOrder() {
@@ -44,71 +44,53 @@ class OrderController extends ChangeNotifier {
   // delete robot
   void deleteMcdRobot(McdRobot robot) {
     robotList.remove(robot);
+    final order = robot.handleOrder;
+    if (order != null) {
+      _resetOrder(order);
+    }
     notifyListeners();
   }
 
   // handler order and create isolate to process concurrency
   Future<void> handleOrder(BasicOrder order, McdRobot robot) async {
+
+    // setting handle time
+    int count = _handleTimes;
+
+    // setting robot handle order
     robot.updateStatus(true);
     robot.setHandleOrder(order);
+
+    // setting order status
     order.inProcess = true;
     order.handleBy = robot;
-    //_queue.remove(order);
-    _createIsolate(order, robot);
+    order.handleStatus = '$count s';
+
+    // process
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      _timer ??= timer;
+      count--;
+      order.handleStatus = '$count s';
+
+      if (count == 0) {
+        // completed order
+        timer.cancel();
+        _timer = null;
+        order.handleStatus = 'complete';
+        order.orderStatus = Status.complete;
+        robot.updateStatus(false);
+        robot.setHandleOrder(null);
+      }
+      notifyListeners();
+    });
   }
 
-  // create isolate
-  // isolate will auto release so no need manual to dispose isolate
-  void _createIsolate(BasicOrder order, McdRobot robot) async {
-    // create a receipt port to communicate with isolate.
-    // because in dart isolate is seal. there are not shared data with main isolate.
-    final ReceivePort receivePort = ReceivePort();
-    // create a send port to receive send port from isolate
-    SendPort? sendPort;
-
-    // listen data from isolate
-    receivePort.listen((value) {
-      BasicOrder handleOrder = order;
-
-      // create communication with isolate.
-      // get the send port from isolate that can receipt isolate data.
-      if (value is SendPort) {
-        sendPort = value;
-      }
-
-      // handler count down, update order status and some business logic
-      if (value is Map<String, dynamic>) {
-        final String? handleStatus = value["handleMessage"];
-        final robotisAvailable = robotList.firstWhereOrNull(
-                (BasicRobot queueRobot) => robot.id == queueRobot.id) !=
-            null;
-        // if robot not available will back to waiting robot handler.
-        if (!robotisAvailable) {
-          sendPort!.send(true);
-          order.orderStatus = Status.pending;
-          order.handleBy = null;
-          order.inProcess = false;
-        }
-        if (handleStatus != null) {
-          handleOrder.handleStatus = handleStatus;
-          notifyListeners();
-        }
-      }
-
-      // if all countdown is completed will update to complete status.
-      if (value is bool) {
-        final complete = value;
-        if (complete) {
-          handleOrder.orderStatus = Status.complete;
-          robot.updateStatus(false);
-          robot.setHandleOrder(null);
-          notifyListeners();
-        }
-      }
-    });
-
-    // run isolate
-    await Isolate.spawn(processOrder, receivePort.sendPort);
+  void _resetOrder(BasicOrder order) {
+    order.orderStatus = Status.pending;
+    order.handleBy = null;
+    order.inProcess = false;
+    order.handleStatus = 'waiting robot handle';
+    _timer!.cancel();
   }
 
   // get first pending order which are haven't process by robot.
@@ -143,42 +125,4 @@ class OrderController extends ChangeNotifier {
   List<BasicOrder> get completeQueue => _queue
       .where((BasicOrder order) => order.orderStatus == Status.complete)
       .toList();
-}
-
-// isolate cannot be class member
-void processOrder(SendPort sendPort) {
-  final ReceivePort receivePort = ReceivePort();
-  bool forceExit = false;
-  int timeout = 10;
-  final Map<String, dynamic> receiveData = {};
-  receivePort.listen((value) {
-    if (value is BasicOrder) {
-      final order = value;
-    }
-    if (value is bool) {
-      forceExit = value;
-    }
-  });
-  sendPort.send(receivePort.sendPort);
-
-  Timer.periodic(const Duration(seconds: 1), (timer) {
-    if (timeout == 0) {
-      timer.cancel();
-    }
-    receiveData["handleMessage"] = '$timeout s';
-    sendPort.send(receiveData);
-    timeout--;
-    if (timeout == -1 || forceExit) {
-      timer.cancel();
-      if (!forceExit) {
-        receiveData["handleMessage"] = 'complete';
-        sendPort.send(receiveData);
-        sendPort.send(true);
-      } else {
-        receiveData["handleMessage"] = 'waiting robot handle';
-        sendPort.send(receiveData);
-        sendPort.send(false);
-      }
-    }
-  });
 }
--- lib/pages/robot.dart
+++ lib/pages/robot.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:se_take_home_assignment/controller/order_controller.dart';
+import 'package:se_take_home_assignment/models/order/vip_order.dart';
+import 'package:se_take_home_assignment/models/robot/basic_robot.dart';
 import 'package:se_take_home_assignment/models/robot/mcd_robot.dart';
+
+import '../models/order/basic_order.dart';
 
 class RobotPage extends StatelessWidget {
   const RobotPage({Key? key, required this.orderController}) : super(key: key);
@@ -8,6 +12,15 @@ class RobotPage extends StatelessWidget {
 
   void _addRobot() {
     orderController.addMcdRobot();
+  }
+
+  String getOrderName(BasicOrder? order) {
+    if (order == null) {
+      return "";
+    }
+    final String orderName;
+    orderName = order is VipOrder ? 'Vip Order ${order.id}' : 'Normal Order ${order.id}';
+    return orderName;
   }
 
   @override
@@ -25,8 +38,16 @@ class RobotPage extends StatelessWidget {
           return ListView.builder(
             itemCount: robotList.length,
             itemBuilder: (BuildContext context, int index) {
+              final McdRobot robot = robotList[index] as McdRobot;
               return ListTile(
-                title: Text('${robotList[index].name} ${robotList[index].id}'),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('${robot.name} ${robot.id}'),
+                    if (robot.handleOrder != null)
+                    Text('Handle ${getOrderName(robot.handleOrder)}'),
+                  ],
+                ),
                 trailing: IconButton(
                   onPressed: () {
                     final robot = robotList[index] as McdRobot;
